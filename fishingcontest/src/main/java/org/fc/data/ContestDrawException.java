99deb9daf19c9e6a1709cf95a8ba4330ddb03b0b
pskopek <pskopek@redhat.com>
2011-09-18 15:24:31 +0200

    Draw finishe (missing file)

---
+++ fishingcontest/src/main/java/org/fc/data/ContestDrawException.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.fc.data;
+
+/**
+ * @author pskopek
+ *
+ */
+public class ContestDrawException extends Exception {
+
+	public ContestDrawException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ContestDrawException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ContestDrawException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ContestDrawException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
