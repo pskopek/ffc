65844a62ccf3cf2c475acf28e3157cfda482bd2b
pskopek <pskopek@redhat.com>
2011-09-25 22:33:58 +0200

    Forgotten class.

---
+++ fishingcontest/src/main/java/org/fc/data/NonRegistered.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.fc.data;
+
+
+/**
+ * @author pskopek
+ *
+ */
+public class NonRegistered {
+
+	public static final Integer[] nonRegisteredList = new Integer[] {
+		41, 21, 31, 58, 48, 35, 
+		26, 33,
+		24,
+		20, // Skrucany Filip
+		57, // Skrucany tomas
+		12 // Adamus
+	};  
+	
+}
